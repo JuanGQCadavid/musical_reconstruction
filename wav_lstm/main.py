60b8c34dff42a5977d41a4049b51bc65b8831369
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2019-05-19 13:45:09 -0500

    model done. workflow missing

--- wav_lstm/main.py
+++ wav_lstm/main.py
@@ -17,25 +17,77 @@ from keras.models import Sequential
 from keras.utils.data_utils import get_file
 from scipy.io import wavfile
 
-def main():
-    rate, data = wavfile.read('songs/hakuna_matata.wav')
-    vocab = sorted(set(data))
-    data = data.astype(str)
-    data = [data[i:i + 25000] for i in range(0, len(data), 25000)]
-    data = [x.tolist() for x in data]
-    
-    print('\nTraining word2vec...')
-    word_model = gensim.models.Word2Vec(data, size=100, min_count=1, window=5, iter=100)
-    pretrained_weights = word_model.wv.syn0
-    vocab_size, emdedding_size = pretrained_weights.shape
-    print('Result embedding shape:', pretrained_weights.shape)
+max_sentence_len = 25000
 
-    def word2idx(word):
-        return word_model.wv.vocab[word].index
-    def idx2word(idx):
-        return word_model.wv.index2word[idx]
-    
-    
+rate, data = wavfile.read('songs/hakuna_matata.wav')
+vocab = sorted(set(data))
+data = data.astype(str)
+data = [data[i:i + 25000] for i in range(0, len(data), 25000)]
+data = [x.tolist() for x in data]
+
+print('\nTraining word2vec...')
+word_model = gensim.models.Word2Vec(data, size=100, min_count=1, window=5, iter=100)
+pretrained_weights = word_model.wv.syn0
+vocab_size, emdedding_size = pretrained_weights.shape
+print('Result embedding shape:', pretrained_weights.shape)
+
+def word2idx(word):
+    return word_model.wv.vocab[word].index
+def idx2word(idx):
+    return word_model.wv.index2word[idx]
+
+print('\nPreparing the data for LSTM...')
+train_x = np.zeros([len(data), max_sentence_len], dtype=np.int32)
+train_y = np.zeros([len(data)], dtype=np.int32)
+for i, sentence in enumerate(data):
+    for t, word in enumerate(sentence[:-1]):
+        train_x[i, t] = word2idx(word)
+    train_y[i] = word2idx(sentence[-1])
+
+print('\nTraining LSTM...')
+model = Sequential()
+model.add(Embedding(input_dim=vocab_size, output_dim=emdedding_size, 
+                    weights=[pretrained_weights]))
+model.add(LSTM(units=emdedding_size))
+model.add(Dense(units=vocab_size))
+model.add(Activation('softmax'))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+
+
+def sample(preds, temperature=1.0):
+  if temperature <= 0:
+    return np.argmax(preds)
+  preds = np.asarray(preds).astype('float64')
+  preds = np.log(preds) / temperature
+  exp_preds = np.exp(preds)
+  preds = exp_preds / np.sum(exp_preds)
+  probas = np.random.multinomial(1, preds, 1)
+  return np.argmax(probas)
+
+def generate_next(text, num_generated=10):
+  word_idxs = [word2idx(word) for word in text.lower().split()]
+  for i in range(num_generated):
+    prediction = model.predict(x=np.array(word_idxs))
+    idx = sample(prediction[-1], temperature=0.7)
+    word_idxs.append(idx)
+  return ' '.join(idx2word(idx) for idx in word_idxs)
+
+def on_epoch_end(epoch, _):
+  print('\nGenerating text after epoch: %d' % epoch)
+  texts = [
+    '1316 5856',
+    '3564 342 1223',
+    '12345 5432',
+    '124',
+  ]
+  for text in texts:
+    sample = generate_next(text)
+    print('%s... -> %s' % (text, sample))
+
+model.fit(train_x, train_y,
+          batch_size=128,
+          epochs=20,
+callbacks=[LambdaCallback(on_epoch_end=on_epoch_end)])
 
 def graph(rate, data):
     time = np.linspace(0, len(data)/rate, num=len(data))
@@ -50,7 +102,3 @@ def show_info(aname, a):
     print ("dtype:", a.dtype)
     print ("min, max:", a.min(), a.max())
     print ()
-
-if __name__ == "__main__": 
-    main()
-    
